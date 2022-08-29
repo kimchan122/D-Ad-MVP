2cc04402de8d017949bc78cd9e9da9092cd49751
kimchan122 <chanchankim122@gmail.com>
2022-08-29 22:04:21 +0900

    video->ipfs

---
+++ src/functions/ObjectToIPFS.js
@@ -0,0 +1,55 @@
+// import axios from 'axios';
+
+const ObjectToIPFS = async (video, obj) => {
+    if (obj) {
+        try {
+            const pinataSDK = require('@pinata/sdk');
+            const pinata = pinataSDK(`${process.env.REACT_APP_PINATA_API_KEY}`, `${process.env.REACT_APP_PINATA_API_SECRET}`);
+            // pinata.testAuthentication().then((result) => {
+            //     console.log(result);
+            // }).catch((err) => {
+            //     console.log(err);
+            // });
+
+            const MyCustomName = "unchainads";
+
+            console.log(video)
+            console.log(obj);
+
+            const body = {
+                message: `${obj.title}`,
+            };
+            const options = {
+                pinataMetadata: {
+                    name: MyCustomName,
+                    keyvalues: {
+                        title: `${obj.title}`,
+                        script: `${obj.script}`,
+                        category: `${obj.category}`,
+                        amount: `${obj.amount}`,
+                        video: `http://unchainads.mypinata.cloud/ipfs/${video}`,
+                    },
+                },
+                pinataOptions: {
+                    cidVersion: 0,
+                },
+            };
+            await pinata.pinJSONToIPFS(body, options).then((result) => {
+                //handle results here
+                console.log(result);
+            }).catch((err) => {
+                //handle error here
+                console.log(err);
+            });
+
+            return true;
+
+        } catch (error) {
+            console.log("Error sending Object to IPFS: ")
+            console.log(error)
+            return false;
+        }
+    }
+}
+
+export default ObjectToIPFS;
---
+++ src/functions/VideoToIPFS.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import ObjectToIPFS from './ObjectToIPFS';
+
+const VideoToIPFS = async (video, obj) => {
+    if (video) {
+        try {
+            const data = new FormData();
+            data.append("file", video);
+            const res = await axios({
+                method: "post",
+                url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    'pinata_api_key': `${process.env.REACT_APP_PINATA_API_KEY}`,
+                    'pinata_secret_api_key': `${process.env.REACT_APP_PINATA_API_SECRET}`,
+                },
+                data: data,
+            });
+
+            const RealHash = res.data.IpfsHash;
+            const VideoHash = `https://api.pinata.cloud/data+${res.data.IpfsHash}`;
+
+            await ObjectToIPFS(RealHash, obj);
+            //return VideoHash;
+            // return true;
+        } catch (error) {
+            console.log("Error sending Video to IPFS: ")
+            console.log(error)
+            return false;
+        }
+    }
+}
+
+export default VideoToIPFS;
